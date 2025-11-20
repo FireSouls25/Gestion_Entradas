7f42e43f3307d22797a6c556d9fcc52927bd5f68
lunamideros2003 <luna.mideros@campusucc.edu.co>
2025-11-19 20:08:17 -0500

    Actualizar estilos y optimizar cÃ³digo

--- users/views.py
+++ users/views.py
@@ -54,19 +54,11 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard:dashboard')
-            else:
-                messages.error(request, 'Usuario o contraseña inválidos.', extra_tags='login_error')
-        else:
-            messages.error(request, 'Usuario o contraseña inválidos.', extra_tags='login_error')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard:dashboard')
+        messages.error(request, 'Usuario o contraseña inválidos.', extra_tags='login_error')
+    return render(request, 'users/login.html')
 
 def logout_view(request):
     logout(request)
